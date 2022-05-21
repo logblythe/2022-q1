9a0264ec2fa9b53e978b37fbe694a664db8fa799
sikshya <sikshya.maharjan@renegadeinsurance.com>
2022-05-21 15:59:37 +0545

    chore: user agent introduced

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,23 @@ import type { NextApiRequest, NextPage } from "next";
 
 export async function getServerSideProps({ req }: { req: NextApiRequest }) {
   const ip = req.headers["x-real-ip"] || req.socket.remoteAddress;
+  const browser = req.headers["user-agent"];
 
   return {
     props: {
       ip,
+      browser,
     }, // will be passed to the page component as props
   };
 }
 
-const Home: NextPage<{ ip: string }> = (props) => {
-  return <div>{props.ip}</div>;
+const Home: NextPage<{ ip: string; browser: string }> = (props) => {
+  return (
+    <div>
+      <div>{props.ip}</div>
+      <div>{props.browser}</div>
+    </div>
+  );
 };
 
 export default Home;
